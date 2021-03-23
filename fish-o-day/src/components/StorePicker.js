11dc6e9864c426d957a3a08d44e9e34c80a2793e
OrkunSA <c.orkun.s@gmail.com>
2021-03-23 15:49:55 -0500

    add events to storepicker

--- fish-o-day/src/components/StorePicker.js
+++ fish-o-day/src/components/StorePicker.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  state = {
+    myInput: "",
+  };
+
+  handleOnChange = (event) => {
+    this.setState({
+      myInput: event.target.value,
+    });
+  };
+
+  goToStore = (event) => {
+    event.preventDefault();
+    const storeName = this.state.myInput;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input
           type="text"
           required
           placeholder="Store Name"
-          defaultValue={getFunName()}
+          value={this.state.myInput}
+          onChange={(e) => this.handleOnChange(e)}
         />
         <button type="submit">Visit Store →</button>
       </form>
